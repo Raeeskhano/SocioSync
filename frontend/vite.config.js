dea31f698dfee37e14858d9bc86403506b1b8863
Raees Khan <rk7523505@gmail.com>
2026-06-13 23:55:30 +0500

    fix: all the warning throwing by vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        }
+      }
+    }
+  },
   server: {
     proxy: {
       '/api': {
